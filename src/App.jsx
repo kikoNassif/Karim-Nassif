ac6bd0f38e515ad11d86e900c5b021a959452a75
KikoNassif <ktamer2003@gmail.com>
2025-07-20 23:45:55 +0400

    Implemented Hero banner section

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,16 @@
  * Compoents
  */
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <Header />
+    <>
+     <Header />
+      <main>
+        <Hero />
+      </main>
+    </>
   )
 }
 
