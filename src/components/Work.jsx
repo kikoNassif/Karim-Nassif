f7e00dce6046a1bccabe8a6504efe64d84e0d975
KikoNassif <ktamer2003@gmail.com>
2025-07-21 23:04:27 +0400

    Updated work (projects) section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,39 +11,39 @@ import ProjectCard from "./ProjectCard";
 const works = [
   {
     imgSrc: '/images/project-1.jpg',
-    title: 'Full stack music app',
+    title: 'Movie database app',
     tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://musify-5al0.onrender.com/'
+    projectLink: 'https://karims-movie-vault.netlify.app/'
   },
   {
     imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
+    title: 'AI image classifier app',
+    tags: ['AI', 'Development'],
     projectLink: 'https://pixstock-official.vercel.app/'
   },
   {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
+    imgSrc: '/images/project-coming.jpg',
+    title: 'Project in progress',
+    tags: ['Coming soon'],
     projectLink: ''
   },
   {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
+    imgSrc: '/images/project-coming.jpg',
+    title: 'Project in progress',
+    tags: ['Coming soon'],
+    projectLink: 'https://github.com/kikoNassif'
   },
   {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
+    imgSrc: '/images/project-coming.jpg',
+    title: 'Project in progress',
+    tags: ['Coming soon'],
+    projectLink: 'https://github.com/kikoNassif'
   },
   {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+    imgSrc: '/images/project-coming.jpg',
+    title: 'Project in progress',
+    tags: ['Coming soon'],
+    projectLink: 'https://github.com/kikoNassif'
   },
 ];
 
