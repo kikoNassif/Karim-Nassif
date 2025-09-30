259e62d5ff96da8d2c1112ba31e95e95b8ddb7ab
KikoNassif <ktamer2003@gmail.com>
2025-09-30 11:04:26 +0400

    Updated hero picture (sitting down)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,7 @@ function Hero() {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[480px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <img 
-            src="/images/hero-banner(1).png" 
+            src="/images/hero-banner2.png" 
             width={656}
             height={800}
             alt="Karim Nassif" 
